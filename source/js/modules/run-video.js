0895957c2dc914febfb18bee0eb2d24fbb7f4d92
Taranovskaya <s5198653@yandex.ru>
2023-11-10 12:32:44 +0300

    Вносит правки по добавлению видео на страницу

--- source/js/modules/run-video.js
+++ source/js/modules/run-video.js
@@ -1,16 +1,26 @@
-const video = document.querySelector('[data-tabs="video"]');
+const videoContainer = document.querySelector('[data-tabs="video-container"]');
 const videoPoster = document.querySelector('[data-tabs="video-poster"]');
 const videoButton = document.querySelector('[data-tabs="video-button"]');
 
+const createIframe = () => {
+  const iframe = document.createElement("iframe");
+  iframe.src = "https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1";
+  iframe.width = "364";
+  iframe.height = "228";
+  iframe.title = "Бесплатные интерактивные онлайн-курсы.";
+  iframe.allow = "fullscreen *";
+  iframe.style.display = "block";
+  videoContainer.appendChild(iframe);
+};
+
 const onClickVideoButton = () => {
-  video.style.display = 'block';
-  video.src += '?feature=oembed&autoplay=1';
-  videoPoster.style.display = 'none';
-  videoButton.style.display = 'none';
+  createIframe();
+  videoPoster.style.display = "none";
+  videoButton.style.display = "none";
 };
 
 const addListnerToVideoButton = () => {
-  videoButton.addEventListener('click', onClickVideoButton);
+  videoButton.addEventListener("click", onClickVideoButton);
 };
 
 const runVideo = () => {
